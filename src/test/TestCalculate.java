535071aeefbfc8844f2ebd9e9fc835b20c0eece6
Patron <patroneq@gmail.com>
2017-06-21 12:05:38 +0200

    test2 red + refactor

--- src/test/TestCalculate.java
+++ src/test/TestCalculate.java
@@ -2,16 +2,28 @@ package test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import main.Calculator;
 
 public class TestCalculate {
+	
+	private Calculator calculator;
+	
+	@Before
+	public void init() {
+		calculator = new Calculator();
+	}
 
 	@Test
 	public void emptyStringReturnsZero() {
-		Calculator calculator = new Calculator();
 		assertEquals(calculator.calculate(""),0);
+	}
+	
+	@Test
+	public void singleValueIsReplied() {
+		assertEquals(calculator.calculate("1"), 1);
 	}
 
 }
