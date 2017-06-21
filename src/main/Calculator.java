854681ff68ef40bd709be330db513f083b9be358
Patron <patroneq@gmail.com>
2017-06-21 12:26:26 +0200

    test4 green

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -3,7 +3,7 @@ package main;
 public class Calculator {
 
 	public int calculate(String input) {
-		String[] numbers = input.split(",");
+		String[] numbers = input.split(",|\n");
 		
 		if(isEmpty(input)) {
 			return 0;
